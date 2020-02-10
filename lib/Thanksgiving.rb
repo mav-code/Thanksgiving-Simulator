c7f901dddc0fac8eb1f8462268977dce169e02d7
mav-code <mavisansmembres@gmail.com>
2020-02-10 17:33:32 -0500

    tables and hwhatnot

---
+++ lib/Thanksgiving.rb
@@ -0,0 +1,2 @@
+class Thanksgiving < ActiveRecord::Base
+end
---
+++ schema.rb
@@ -0,0 +1,3 @@
+CREATE TABLE people (id INTEGER PRIMARY KEY, name TEXT, hunger INTEGER, tryptophan INTEGER, politics TEXT);
+CREATE TABLE plate (id INTEGER PRIMARY KEY);
+CREATE TABLE thanksgiving (id INTEGER PRIMARY KEY, location TEXT, type TEXT, strife INTEGER);
