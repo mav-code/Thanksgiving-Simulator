f7f641f3c820dc47192947c6071cb508b4c12467
github username <github email address>
2020-02-12 09:46:08 -0500

    locations and person added

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,15 @@ require_relative '../config/environment'
 require 'faker'
 
 # jemy = Person.create(name: 'Jemy', hunger: 8, tryptophan: 10, politics: 'blue')
-Person.create(name: Faker::Name.name, hunger: 10, tryptophan: 10, politics: "red")
+Person.create(name: Faker::Name.name, hunger: 10, tryptophan: 10, politics: "Red")
+Person.create(name: Faker::Name.name, hunger: 10, tryptophan: 10, politics: "Blue")
+Person.create(name: Faker::Name.name, hunger: 10, tryptophan: 10, politics: "Blue")
+Person.create(name: Faker::Name.name, hunger: 10, tryptophan: 10, politics: "Red")
+Person.create(name: Faker::Name.name, hunger: 10, tryptophan: 10, politics: "Red")
+Person.create(name: Faker::Name.name, hunger: 10, tryptophan: 10, politics: "Blue")
+
+brooklyn = Thanksgiving.create!(type: "Turkey", strife: 0, location: "Brooklyn")
+new_brunswick = Thanksgiving.create!(type: "Tofukey", strife: 0, location: "New Brunswick")
+queens = Thanksgiving.create!(type: "Turkey", strife: 0, location: "Queens")
+bronx = Thanksgiving.create!(type: "Tofukey", strife: 0, location: "Bronx")
+s_i = Thanksgiving.create!(type: "Tofukey", strife: 0, location: "Staten Island")
