687f96d7896017d84f7f4c782e9970a4d5bcc523
mav-code <mavisansmembres@gmail.com>
2020-02-12 14:20:47 -0500

    fleshed out menuflow

--- lib/interface.rb
+++ lib/interface.rb
@@ -50,3 +50,70 @@ def intro
     sleep (5)
     puts "\n"
 end
+
+def thanksgiving_menu
+    prompt = TTY::Prompt.new
+    display_current_plates
+    tg_choice = prompt.select("Which feast are you thinking of hitting?", Thanksgiving.all.map{|tg| tg.location})
+    current_tg = Thanksgiving.find_by location tg_choice.to_s
+    sleep(0.5)
+    puts "#{tg_choice}. You start gathering your things."
+    sleep(3)
+    call_menu_choice = prompt.select(["Go to #{tg_choice}", "Call ahead", "Back"])
+    if call_menu_choice == "Back"
+        thanksgiving_menu
+    elsif call_menu_choice == "Call ahead"
+        call(current_tg)
+    else
+        attend(current_tg)
+    end
+end
+
+def call(tg)
+    puts "You decide to call ahead. >> Call text will go here once we've populated the thanksgiving. <<"
+    call_choice = prompt.select(["Go to #{tg.location}.", "Back"])
+    if call_choice == "Back"
+        thanksgiving_menu
+    else
+        attend(tg)
+    end
+        
+end
+
+def attend(tg)
+    "You arrive at #{tg.location}. >> Will develop more trinket text to go here. <<"
+
+    if tg.course == "Tofurkey"
+        puts "It's vegetarian this year."
+    end
+    if user.thanksgivings.any?{|thanksgiving| thanksgiving = tg}
+        puts "You find your plate. Someone has put out a cigarette on it."
+    else
+        puts "You fix yourself a plate and commence small talk. You have fulfilled your harvest pact with these people."
+        Plate.create(person_id: user.id, thanksgiving_id: tg.id)
+    end
+
+    plate_choice = prompt.select(["Get into a political argument", "Try and attend another Thanksgiving"])
+        if plate_choice == "Get into a political argument"
+            political_argument
+        else
+            thanksgiving_menu
+        end
+
+end
+
+def political_argument
+    puts "I haven't written this part yet."
+    prompt.select(["Attend another Thanksgiving."])
+    thanksgiving_menu
+end
+
+def display_current_plates
+    if Plate.all.any?{|plate| plate.person_id == user.id}
+        locations = user.plates.map{plate.thanksgiving.location}
+        if locations.length == 1
+            puts "You have a plate at #{locations}."
+        else puts "You have plates at #{locations}."
+        end
+    end
+end
