4f28ac5e587ed62d7a2ce894ab4fe81238cbd328
github username <github email address>
2020-02-12 10:42:08 -0500

    fixed

--- bin/run.rb
+++ bin/run.rb
@@ -3,12 +3,12 @@ require 'faker'
 
 # this is where our actual application will run, 
 # but it'll just be calling code in other places
-arrayoffiverandomlygeneratedthanksgivings = ["placeholder1", "placeholder2", "placeholder3", "placeholder4", "placeholder5"]
+thanksgivings = [brooklyn, new_brunswicks, queens, manhattan, s_i]
 prompt = TTY::Prompt.new
 puts "hello world"
 
 intro
-tg_choice = prompt.select("Which feast are you thinking of hitting?", arrayoffiverandomlygeneratedthanksgivings)
+tg_choice = prompt.select("Which feast are you thinking of hitting?", thanksgivings.pluck(:location))
 
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@ Person.create(name: Faker::Name.name, hunger: 10, tryptophan: 10, politics: "Red
 Person.create(name: Faker::Name.name, hunger: 10, tryptophan: 10, politics: "Red")
 Person.create(name: Faker::Name.name, hunger: 10, tryptophan: 10, politics: "Blue")
 
-brooklyn = Thanksgiving.create!(type: "Turkey", strife: 0, location: "Brooklyn")
-new_brunswick = Thanksgiving.create!(type: "Tofukey", strife: 0, location: "New Brunswick")
-queens = Thanksgiving.create!(type: "Turkey", strife: 0, location: "Queens")
-bronx = Thanksgiving.create!(type: "Tofukey", strife: 0, location: "Bronx")
-s_i = Thanksgiving.create!(type: "Tofukey", strife: 0, location: "Staten Island")
+brooklyn = Thanksgiving.create!(course: "Turkey", strife: 0, location: "Brooklyn")
+new_brunswick = Thanksgiving.create!(course: "Tofukey", strife: 0, location: "New Brunswick")
+queens = Thanksgiving.create!(course: "Turkey", strife: 0, location: "Queens")
+bronx = Thanksgiving.create!(course: "Tofukey", strife: 0, location: "Bronx")
+s_i = Thanksgiving.create!(cours: "Tofukey", strife: 0, location: "Staten Island")
