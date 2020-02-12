f4008cef2221836c2b4a4d71b6455fb01ebbc635
github username <github email address>
2020-02-12 11:22:17 -0500

    .

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,4 @@ brooklyn = Thanksgiving.create!(course: "Turkey", strife: 0, location: "Brooklyn
 new_brunswick = Thanksgiving.create!(course: "Tofukey", strife: 0, location: "New Brunswick")
 queens = Thanksgiving.create!(course: "Turkey", strife: 0, location: "Queens")
 bronx = Thanksgiving.create!(course: "Tofukey", strife: 0, location: "Bronx")
-<<<<<<< HEAD
-s_i = Thanksgiving.create!(cours: "Tofukey", strife: 0, location: "Staten Island")
-=======
 s_i = Thanksgiving.create!(course: "Tofukey", strife: 0, location: "Staten Island")
->>>>>>> master
