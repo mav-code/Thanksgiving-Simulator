e5f088eddc91b2d7d5555f8566f860af320fde57
github username <github email address>
2020-02-12 11:06:21 -0500

    merge

--- bin/run.rb
+++ bin/run.rb
@@ -3,17 +3,14 @@ require 'faker'
 
 # this is where our actual application will run, 
 # but it'll just be calling code in other places
-thanksgivings = [brooklyn, new_brunswicks, queens, manhattan, s_i]
+
+#thanksgivings = [brooklyn, new_brunswicks, queens, manhattan, s_i]
 prompt = TTY::Prompt.new
 puts "hello world"
 
 intro
-<<<<<<< HEAD
-tg_choice = prompt.select("Which feast are you thinking of hitting?", thanksgivings.pluck(:location))
-=======
 tg_choice = prompt.select("Which feast are you thinking of hitting?", Thanksgiving.all.map{|tg| tg.location})
 
->>>>>>> master
 
 
 
