675e5827b9f5739c5deb26e21b00ef6a1e21a921
mav-code <mavisansmembres@gmail.com>
2020-02-12 20:31:43 -0500

    improved spacing and timing of cli

--- lib/interface.rb
+++ lib/interface.rb
@@ -49,13 +49,13 @@ def intro
     puts "\n"
     puts "The year is 2020, the month November. Halloween is over. You promise yourself your costume will be better next year."
     puts "\n"
-    sleep(7)
+    sleep(3)
     puts "The election was three weeks ago. It was devastating."
     puts "\n"
-    sleep(5)
+    sleep(3)
     puts "It is time once again for the American harvest/genocide festival."
     puts "\n"
-    sleep(5)
+    sleep(3)
     dish = prompt.ask('What did you make to bring to Thanksgiving this year?')
     sleep(1)
     puts "\n"
@@ -64,19 +64,22 @@ def intro
     else 
     puts "Nothing, huh? Typical."
     end
-    sleep(3)
+    sleep(2)
     puts "\n"
     puts "This year's a big one; you're expected at five seperate Thanksgivings. It's a real ordeal, but you suppose you should be thankful."
-    sleep (5)
-    puts "\n"
+    sleep (0.5)
 end
 
 def thanksgiving_menu
     prompt = TTY::Prompt.new
+    sleep(0.5)
     display_current_plates
+    sleep(0.5)
+    puts "\n"
     tg_choice = prompt.select("Which feast are you thinking of hitting?", Thanksgiving.all.map{|tg| tg.location})
     current_tg = Thanksgiving.find_by location: tg_choice.to_s
     sleep(0.5)
+    puts "\n"
     call_menu_choice = prompt.select("#{tg_choice}. You start gathering your things.", ["Go to #{tg_choice}", "Call ahead", "Back"])
     if call_menu_choice == "Back"
         thanksgiving_menu
@@ -89,6 +92,8 @@ end
 
 def call(tg)
     prompt = TTY::Prompt.new
+    sleep(0.5)
+    puts "\n"
     call_choice = prompt.select("You decide to call ahead. >> Call text will go here once we've populated the thanksgiving. <<",["Go to #{tg.location}.", "Back"])
     if call_choice == "Back"
         thanksgiving_menu
@@ -103,8 +108,12 @@ def attend(tg)
     "You arrive at #{tg.location}. >> Will develop more trinket text to go here. <<"
 
     if tg.course == "Tofurkey"
+        sleep(0.5)
+        puts "\n"
         puts "It's vegetarian this year."
     end
+    sleep(0.5)
+    puts "\n"
     if $user.thanksgivings.all.any?{|thanksgiving| thanksgiving == tg}
         puts "You find your plate. Someone has put out a cigarette on it."
     else
@@ -112,6 +121,8 @@ def attend(tg)
         newplate = Plate.create(person_id: $user.id, thanksgiving_id: tg.id)
         newplate.save
     end
+    sleep(0.5)
+    puts "\n"
     plate_choice = prompt.select("What's next?", ["Get into a political argument", "Try and attend another Thanksgiving"])
         if plate_choice == "Get into a political argument"
             political_argument(tg)
@@ -123,21 +134,31 @@ end
 
 def political_argument(tg)
     prompt = TTY::Prompt.new
+    sleep(1)
+    puts "\n"
     argue_choice = prompt.select("You start hollering about electoralism, the Second Internationale, and podcasts.", ["Calm down!", "Smash your plate and get the heck out of here!"])
     if argue_choice == "Calm down!"
         attend(tg)
     else
+        sleep(0.5)
+        puts "\n"
         puts "With one finger shaking in the air, as if to accuse God herself, you pound your other meaty fist into your mashed potatoes. The dish shatters."
-
+        sleep(3)
+        puts "\n"
         puts "No one responds or looks you in the eye."
-
+        sleep(3)
+        puts "\n"
         puts "Let's get out of here."
+        sleep(3)
+        puts "\n"
         Plate.where(person_id: $user.id, thanksgiving_id: tg.id)[0].destroy
         thanksgiving_menu
     end
 end
 
 def display_current_plates
+    sleep(0.5)
+    puts "\n"
     if Plate.all.any?{|plate| plate.person_id == $user.id}
         locations = $user.plates.all.map{|plate| plate.thanksgiving.location}
         if locations.length == 1
